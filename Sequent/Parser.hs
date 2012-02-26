cd565fe1bec2193b59ce6225fc9b2adc17fbee23
Luke Palmer <lrpalmer@gmail.com>
2012-02-26 13:33:47 -0700

    Added parser.

---
+++ Sequent/Parser.hs
@@ -0,0 +1,46 @@
+module Sequent.Parser where
+
+import Prelude hiding (lex)
+import Control.Applicative
+import Data.Either (partitionEithers)
+import Data.List (intercalate)
+import qualified Text.Parsec as P
+import qualified Text.Parsec.Token as P
+import qualified Text.Parsec.Language as P
+import Sequent.Syntax
+
+type Parser = P.Parsec String ()
+
+lex = P.makeTokenParser P.haskellDef
+
+clauseAtom :: Parser ClauseAtom
+clauseAtom = P.choice [
+    var,
+    typ,
+    rel ]
+    where
+    var = AVar <$> P.identifier lex
+    typ = AType <$> atomExpr <* P.symbol lex ":" <*> expr
+    rel = convert <$> P.brackets lex (P.many relAtom)
+        where
+        relAtom = P.choice [
+                Left <$> P.identifier lex,
+                Right <$> P.parens lex expr
+            ]
+        convert xs = let (names, args) = partitionEithers xs in
+                     ARel (intercalate "_" names) args
+
+clause :: Parser Clause
+clause = convert <$> P.many clauseAtom <* P.symbol lex "->" <*> P.many clauseAtom
+    where
+    convert hyps cons = ImplClause (zip [0..] hyps) (zip [0..] cons)
+
+expr :: Parser Expr
+expr = atomExpr
+
+atomExpr :: Parser Expr
+atomExpr = P.choice [
+        VarExpr <$> P.identifier lex,
+        ApplyExpr <$> P.parens lex (P.many1 atomExpr)
+    ]
+
