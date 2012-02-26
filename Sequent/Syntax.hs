59f39cea552853401f030e8461fa381e3e0dd1a8
Luke Palmer <lrpalmer@gmail.com>
2012-02-26 01:06:56 -0700

    Added display functions.

--- Sequent/Syntax.hs
+++ Sequent/Syntax.hs
@@ -1,5 +1,6 @@
 module Sequent.Syntax where
 
+
 type Name = String
 type Label = Integer
 
@@ -22,3 +23,29 @@ data Expr
     = VarExpr Name
     | ApplyExpr [Expr]
     deriving Eq
+
+showAtom :: ClauseAtom -> String
+showAtom (AVar n) = n
+showAtom (AType e t) = showExpr True e ++ ":" ++ showExpr True t
+showAtom (ARel n xs) = "[" ++ expandRel n (map (showExpr True) xs) ++ "]"
+showAtom (AClause c) = "(" ++ showClause c ++ ")"
+
+showExpr :: Bool -> Expr -> String
+showExpr parens (VarExpr n) = n
+showExpr parens (ApplyExpr es) = p $ unwords (map (showExpr True) es)
+    where
+    p x | parens = "(" ++ x ++ ")"
+        | otherwise = x
+
+expandRel :: String -> [String] -> String
+expandRel ('_':ns) (s:ss) = s ++ expandRel ns ss
+expandRel ('_':ns) [] = error "Relation arity too small"
+expandRel (n:ns) ss = n : expandRel ns ss
+expandRel [] (s:ss) = error "Relation arity too large"
+expandRel [] [] = []
+
+showClause :: Clause -> String
+showClause (ImplClause hyps cons) = shyps ++ " -> " ++ scons
+    where
+    shyps = unwords $ map (showAtom.snd) hyps
+    scons = unwords $ map (showAtom.snd) cons
