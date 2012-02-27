a211967cca93813500e5872936395da9b178f9d6
Luke Palmer <lrpalmer@gmail.com>
2012-02-26 20:23:12 -0700

    Super-abstractified the proof data structure.

--- Sequent/Environment.hs
+++ Sequent/Environment.hs
@@ -20,10 +20,10 @@ type Pf = Mu PfLink
 
 data Environment = Environment {
     clause :: Clause,
-    proof :: Pf
+    goals :: [(Clause, Pf -> Pf)]
 }
 
-type SuspendChecker = Const [(Clause, Pf -> Pf)]
+type Obligations = Const [(Clause, Pf -> Pf)]
 
 
 {-
@@ -32,43 +32,44 @@ proofCheck1 :: Proof (Checker f) -> Checker f
 -}
 
 
-proofCheck :: Pf -> Proof.Checker SuspendChecker
+mapConst :: (b -> c) -> Const b a -> Const c a
+mapConst f (Const b) = Const (f b)
+
+proofCheck :: Pf -> Proof.Checker Obligations
 proofCheck c = cata scheck c
     where
-    scheck :: PfLink (Proof.Checker SuspendChecker) -> Proof.Checker SuspendChecker
+    scheck :: PfLink (Proof.Checker Obligations) -> Proof.Checker Obligations
     scheck (PfLink (Suspend ())) c = return $ Const [(c, id)]
-    scheck (PfLink (Normal p)) c = Proof.proofCheck1 p c
+    -- Maybe (Const [(Clause, Pf -> Pf)]
+    -- p :: Proof (Proof.Checker Obligations)
+    -- fmap.const :: Pf -> Pf
+    scheck (PfLink (Normal p)) c = (fmap.mapConst.fmap.fmap.fmap.xform) p (Proof.proofCheck1 p c)
 
-{-
-toConts :: Environment -> [(Clause, Proof () -> Environment)]
-toConts env = [ (c, Environment (clause env) . cont hc) | (c, hc) <- holes ]
-    where
-    checked = proofCheck (clause env) (proof env)
-    holes = holeConts checked
+    xform :: Proof.Proof a -> Pf -> Pf
+    xform p q = Roll . PfLink . Normal $ fmap (const q) p
+
 
 newtype IOEnv = IOEnv (IORef Environment)
 
-proofIO :: IOEnv -> IO (Proof ())
-proofIO (IOEnv r) = proof <$> readIORef r
-
 newIOEnv :: Clause -> IO IOEnv
-newIOEnv c = IOEnv <$>  newIORef (Environment c (Suspend ()))
+newIOEnv c = IOEnv <$> newIORef (Environment c [(c, id)])
 
 display :: IOEnv -> IO ()
 display (IOEnv r) = do
     env <- readIORef r
-    let conts = toConts env
-    forM_ (zip [0..] conts) $ \(i,(c,_)) -> 
+    forM_ (zip [0..] (goals env)) $ \(i,(c,_)) -> 
         putStrLn $ show i ++ ":: " ++ showClauseV c
 
-applyTactic :: IOEnv -> Int -> Proof () -> IO ()
-applyTactic (IOEnv r) i pf = do
-    env <- readIORef r
-    let conts = toConts env
-    when (i < length conts) $ do 
-        let env' = snd (conts !! i) pf
-        writeIORef r env'
+tactic :: Proof.Proof () -> Pf
+tactic = Roll . PfLink . Normal . fmap (const (Roll (PfLink (Suspend ()))))
 
-tactic :: IOEnv -> Int -> Proof () -> IO ()
-tactic env i pf = applyTactic env i pf >> display env
--}
+a :: IOEnv -> Int -> Pf -> IO ()
+a (IOEnv r) i pf = do
+    env <- readIORef r
+    let conts = goals env
+    when (i < length conts) $ do 
+        let pf' = snd (conts !! i) pf
+        case proofCheck pf' (clause env) of
+            Nothing -> fail "Tactic failed"
+            Just (Const obs) -> writeIORef r (Environment (clause env) obs)
+    display (IOEnv r)
