a59b03c452b401376cb1a70e54e43aa1bf9475b0
Luke Palmer <lrpalmer@gmail.com>
2012-02-29 22:29:45 +0000

    Prettified generated javascript.  Can always minify later.

--- Sequent/Program.hs
+++ Sequent/Program.hs
@@ -2,6 +2,7 @@ module Sequent.Program where
 
 import qualified Data.Char as Char
 import Data.List (intercalate)
+import qualified Numeric
 
 type Hyp = String
 type Goal = String
@@ -15,36 +16,42 @@ data Program
             Program [(Hyp, Hyp)]   -- variables for p2
                     [(Goal, Hyp)]  -- map goals of p2 into our new hyps
 
+indent :: String -> String -> String
+indent by = unlines . map (by ++) . lines
+
 quoteJS :: String -> String
-quoteJS = ("__" ++) . concatMap trans
+quoteJS s
+    | Char.isAlpha (head s) = concatMap trans s
+    | otherwise = "$" ++ concatMap trans s
     where
     trans '_' = "__"
     trans '(' = "_E"
     trans ')' = "_D"
-    trans ',' = "_I"
+    trans ',' = "_j"
     trans '.' = "_o"
-    trans x = [x]
+    trans x | Char.isAlphaNum x = [x]
+    trans x = "_O" ++ pad 3 '0' (Numeric.showOct (Char.ord x) "")
+
+    pad n c s = replicate (length s - n) c ++ s
 
 toJS :: Program -> String
 toJS (Init hyps p) = 
-    "function (_hyps) {\n" ++
-    "var _goals = {};\n" ++
-    unlines [ "var " ++ quoteJS h ++ " = _hyps." ++ quoteJS h ++ ";" | h <- hyps ] ++
-    toJS p
+    "function (_hyps) {\n" ++ indent "  " (
+        "var _goals = {};\n" ++
+        unlines [ "var " ++ quoteJS h ++ " = _hyps." ++ quoteJS h ++ ";" | h <- hyps ] ++
+        toJS p
+    ) ++ "}"
 toJS Return =
-    "return _goals;\n" ++
-    "}"
+    "return _goals;\n"
 toJS (SetResult g h p) =
-    "_goals." ++ quoteJS g ++ " = " ++ quoteJS h ++ ";" ++
+    "_goals." ++ quoteJS g ++ " = " ++ quoteJS h ++ ";\n" ++
     toJS p
 toJS (Apply f helper helpermap p vars goalmap) =
     "var _tmp = " ++ fapply ++ ";\n" ++
     unlines [ "var " ++ quoteJS res ++ " = _tmp." ++ quoteJS rparam | (rparam, res) <- goalmap ] ++
     toJS p
     where
-    fapply = 
-        quoteJS f ++ "(_adapt(" ++ toJS (Init [] helper) ++ "(), " 
-                  ++ adapter helpermap ++ ", "
-                  ++ object vars ++ "))"
+    fapply = quoteJS f ++ "(_adapt(" ++ adapter helpermap ++ ", " ++ object vars ++ ", " ++ 
+                  toJS (Init [] helper) ++ "()))"
     adapter xs = "{ " ++ intercalate ", " [ quoteJS k ++ ": '" ++ quoteJS v ++ "'" | (k,v) <- xs ] ++ " }"
     object xs = "{ " ++ intercalate ", " [ quoteJS k ++ ": " ++ quoteJS v | (k,v) <- xs ] ++ " }"
